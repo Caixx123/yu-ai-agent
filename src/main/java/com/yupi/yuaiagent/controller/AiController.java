ee8ab40058f85b88eeb6e46079f0bd44a9e4b575
Caixx123 <caixx321@163.com>
2026-03-19 16:32:49 +0800

    服务化

---
+++ src/main/java/com/yupi/yuaiagent/controller/AiController.java
@@ -0,0 +1,99 @@
+package com.yupi.yuaiagent.controller;
+
+import com.yupi.yuaiagent.agent.YuManus;
+import com.yupi.yuaiagent.app.LoveApp;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/ai")
+public class AiController {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Resource
+    private ToolCallback[] allTools;
+
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    /**
+     * 同步接口
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping("/love_app/chat/sync")
+    public String doChatWithLoveAppSync(String message, String chatId) {
+        return loveApp.doChat(message, chatId);
+    }
+
+    /**
+     * 编写基于SSE的流式输出接口，有几种常见的实现方式：
+     * 1)返回Flux响应式对象，并且添加SSE对应的MediaType:
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping(value = "/love_app/chat/sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> doChatWithLoveAppSSE(String message, String chatId) {
+        return loveApp.doChatByStream(message, chatId);
+    }
+
+
+    /**
+     * 编写基于SSE的流式输出接口，有几种常见的实现方式：
+     * 3)使用SSEEmite，通过send方法持续向SseEmitter发送消息（有点像IO操作）：
+     * @param message
+     * @param chatId
+     * @return
+     */
+    @GetMapping("/love_app/chat/sse/emitter")
+    public SseEmitter doChatWithLoveAppSseEmitter(String message, String chatId) {
+        // 创建一个超时时间较长的 SseEmitter
+        SseEmitter emitter = new SseEmitter(180000L); // 3分钟超时
+        // 获取 Flux 数据流并直接订阅
+        loveApp.doChatByStream(message, chatId)
+                .subscribe(
+                        // 处理每条消息
+                        chunk -> {
+                            try {
+                                emitter.send(chunk);
+                            } catch (IOException e) {
+                                emitter.completeWithError(e);
+                            }
+                        },
+                        // 处理错误
+                        emitter::completeWithError,
+                        // 处理完成
+                        emitter::complete
+                );
+        // 返回emitter
+        return emitter;
+    }
+
+    /**
+     * 流式调用 Manus 超级智能体
+     *
+     * @param message
+     * @return
+     */
+    @GetMapping("/manus/chat")
+    public SseEmitter doChatWithManus(String message) {
+        YuManus yuManus = new YuManus(allTools, dashscopeChatModel);
+        return yuManus.runStream(message);
+    }
+
+
+}
